4433431bd20ddbdde92b0a9e252143071953f4b6
kpnguyen9 <kpnguyen2@gmail.com>
2021-12-11 12:07:39 -0600

    initial js commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// function helloWorld() {
+//   console.log("Hello World");
+// }
+
+// console.log(4 * 2);
+// console.log(2 + 2);
+
+// const greet = "Hello";
+
+// function greeting(name) {
+//   return "Hello" + " " + name;
+// }
+
+// console.log(greeting("Batman"));
+
+// const myName = "John";
+// const yourName = "Batman";
+
+// function canWe() {
+//   console.log(myName, yourName);
+// }
+
+// canWe();
+
+//Arrays
+// const Arr = ["Hi", 1, 4, "apple"];
+// const Arr2 = [14, "two", 56, "twenty"];
+
+// //Objects
+// const obj = {
+//   name: "John",
+//   age: 30,
+//   city: "Houston",
+// };
+
+// console.log(obj);
+// console.log(obj.name);
+
+// const firstNumber = 23;
+// const secondNumber = 45;
+// const answer = firstNumber + secondNumber;
+
+// console.log(`The answer is ${answer}`);
+
+// let sum = 3 + 4;
+// let sub = 10 - 2;
+// let mult = 2 * 3;
+// let div = 10 / 2;
+// let mod = 25 % 2;
+
+// console.log(sum, sub, mult, div, mod);
+
+function add(a, b) {
+  //add input values
+  return a + b;
+}
+console.log(add(2, 3));
